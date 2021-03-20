3ed8e16556ba2360338df76559e3d50be7abccd1
sankhanilsen <76916705+sankhanilsen@users.noreply.github.com>
2021-03-20 21:46:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function take_snapshot(){
     });
 }
 console.log('ml5 version:',ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/uSecu_ZBq/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZMqaLijpY/model.json',modelLoaded);
 function modelLoaded(){
     console.log('modelLoaded');
 }
